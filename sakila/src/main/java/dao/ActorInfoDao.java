f9ff165c4ed34222091a69253805645680e2175b
Park <Park@DESKTOP-RNAF61E>
2022-04-04 14:46:13 +0900

    [박희원] actorInfoList, ActorInfo, ActorInfoDao 추가 완료

--- sakila/src/main/java/dao/ActorInfoDao.java
+++ sakila/src/main/java/dao/ActorInfoDao.java
@@ -5,33 +5,103 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import util.DBUtil;
 import vo.ActorInfo;
 
 public class ActorInfoDao {
+	
 	public List<ActorInfo> selectActorInfoListByPage(int beginRow, int rowPerPage) {
 		List<ActorInfo> list = new ArrayList<ActorInfo>();
+		
 		Connection conn = null;
 		conn = DBUtil.getConnection();
-		String sql = "SELECT * FROM actor_info ORDER BY actor_id LIMIT ?, ?";
+		
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
+		String sql = "SELECT * FROM actor_info ORDER BY actor_id LIMIT ?, ?";
+		
 		try {
 			stmt = conn.prepareStatement(sql);
-			// ?
+			stmt.setInt(1, beginRow);
+			stmt.setInt(2, rowPerPage);
 			rs = stmt.executeQuery();
-			// rs.next()  .... list.add
+			
+			while(rs.next()) {
+				ActorInfo actorInfo = new ActorInfo();
+				actorInfo.setActorId(rs.getInt("actor_id"));
+				actorInfo.setFirstName(rs.getString("first_name"));
+				actorInfo.setLastName(rs.getString("last_name"));
+				actorInfo.setFilmInfo(rs.getString("film_info"));
+				System.out.println(actorInfo.toString() + "actor_info");
+				list.add(actorInfo);
+			}		
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				rs.close(); stmt.close(); conn.close();
+				rs.close();
+				stmt.close();
+				conn.close();
+				
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
+	
+		
 		return list;
 	}
+	
+
+
+
+	public int lastPage (int beginRow, int rowPerPage) {
+		int totalRow = 0; 
+		int lastPage = 0;
+		
+		Connection conn = null;
+		conn = DBUtil.getConnection();
+		
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = "select count(*) cnt from actor_info";
+		
+		try {
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			if(rs.next()) {
+				totalRow = rs.getInt("cnt");
+			}
+			if(totalRow % rowPerPage == 0) {
+				lastPage = totalRow / rowPerPage;
+			} else {
+				lastPage = (totalRow / rowPerPage) + 1;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		}
+		
+		return lastPage;
+	}
+		
 }
+
+
+
+
+
---
+++ sakila/src/main/webapp/actorInfoList.jsp
@@ -0,0 +1,98 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import = "java.sql.*" %>
+<%@ page import = "java.util.*" %>
+<%@ page import = "vo.*" %>
+<%@ page import = "dao.*" %>
+<%@ page import = "util.*" %>
+<%
+	
+	int currentPage = 1;
+	
+	if(request.getParameter("currentPage") != null) { 
+		 currentPage = Integer.parseInt(request.getParameter("currentPage"));
+	}	
+	System.out.println(currentPage + "<-- currentPage");
+	
+	
+	int rowPerPage = 10;
+	int beginRow = (currentPage-1) * rowPerPage;
+	
+	
+	ActorInfoDao actorinfoDao = new ActorInfoDao();
+	
+	
+	List<ActorInfo> list = actorinfoDao.selectActorInfoListByPage(beginRow, rowPerPage); 
+	int lastPage = actorinfoDao.lastPage(beginRow, rowPerPage);
+
+%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Actor Info List</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+</head>
+<div class="container">
+<body>
+	
+	
+	<a href="<%=request.getContextPath()%>/index.jsp" class="btn btn-outline-dark btn-sm" role="button">홈으로</a>
+	
+	<h1 class="text-center">Actor Info List</h1>
+	<table class="table">
+
+			<tr class = "table-dark">
+				<th class="text-dark" >ActorId</th>
+				<th class="text-dark">firstName</th>
+				<th class="text-dark">lastName</th>
+				<th class="text-dark">filmInfo</th>	
+			</tr>
+
+			<%
+				for(ActorInfo a : list) {
+			
+			%>
+				<tr>
+			      <td><%=a.getActorId()%></td>
+			      <td><%=a.getFirstName()%></td>
+			      <td><%=a.getLastName()%></td>
+			      <td><%=a.getFilmInfo()%></td>
+				</tr>
+	  		
+	  		<%
+	  			}
+	  		%>		
+	</table>
+	<br>
+	<div class="btn-group">
+		<%
+			if(currentPage > 1) { 
+		%>
+				<ul class="pagination">
+				<li class="page-item">
+				<a href="<%=request.getContextPath()%>/actorInfoList.jsp?currentPage=<%=currentPage-1%>" class="btn btn-secondary btn-sm" role="button">이전</a>
+				</li></ul>
+				
+		<%	
+			}
+		%>
+		
+		<%
+	
+			if(currentPage < lastPage) {
+		%>
+				<ul class="pagination">
+				<li class="page-item">
+				<a href="<%=request.getContextPath()%>/actorInfoList.jsp?currentPage=<%=currentPage+1%>" class="btn btn-secondary btn-sm" role="button">다음</a>
+				</li></ul>
+		<%		
+			}
+		%>
+	</div>
+	</div>
+	
+	
+
+</body>
+</html>
